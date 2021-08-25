cbe8dd06e8ee428812123909a70e40e6a2c774aa
Shayn Hacker <hacker0prime@gmail.com>
2021-08-25 00:07:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nesting
+ Insert a website into another website due to issues with maintaining ssl certificate with forward masking with Godaddy
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        html, body{
+            height: 100vh;
+        }
+    </style>
+</head>
+<body>
+<!-- https://stackoverflow.com/questions/36819128/embed-external-website-in-html5 -->
+      <embed src="https://www.questionpro.com/t/7BmgUEZoMuz" width="100%" height="100%"> </embed>
+</object>
+</body>
+</html>
